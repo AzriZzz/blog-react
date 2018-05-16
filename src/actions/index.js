a0515edd25d9998bb19283e6a2a1258dda84dff0
Muhammad Azri <lenovorosak@gmail.com>
2018-05-16 08:43:07 +0800

    add api key, root url

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'https://reduxblog.herokuapp.com/';
+const API_KEY = '?key=AZRI9090'; 
+
+export function fetchPosts() {
+    const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
+    return{
+        type: FETCH_POSTS
+    };
+}
