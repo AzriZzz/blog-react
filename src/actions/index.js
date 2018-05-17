a72ad3e05ea0326f97b0f359f481397f966c26c6
Muhammad Azri <lenovorosak@gmail.com>
2018-05-18 06:18:25 +0800

    implementing posts reducer and add action reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,14 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 
-const ROOT_URL = 'https://reduxblog.herokuapp.com/';
+const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=AZRI9090'; 
 
 export function fetchPosts() {
     const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
 
     return{
-        type: FETCH_POSTS
+        type: FETCH_POSTS,
+        payload: request
     };
 }
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchPosts, FETCH_POSTS } from '../actions';
 
 
 class PostsIndex extends Component{
-     render() {
-         return(
-            <div>
-                Posts Index
-            </div>
-         );
-     }
+    componentDidMount(){
+        this.props.fetchPosts();
+    }
+
+    render() {
+        return(
+        <div>
+            Posts Index
+        </div>
+        );
+    }
 }
 
-
-export default PostsIndex;
+// { fetchPosts: fetchPosts } == { fetchPosts } es6 command
+export default connect( null, {fetchPosts} )(PostsIndex); 
