96cebd08ba545189133072efb80d6a7482e5fc99
Muhammad Azri <lenovorosak@gmail.com>
2018-05-19 22:10:30 +0800

    validate the forms

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -39,7 +39,33 @@ class PostsNew extends Component {
     }
 }
 
+function validate(values) {
+    const errors = {};
+
+    //validate the input from the 'values'
+    if (values.title.length < 3) {
+        errors.title = 'Title must be at least 3 characters!';
+    }
+
+    if (!values.title || values.title.length < 3){
+        errors.title = 'Enter a title!';
+    }
+
+    if (!values.content){
+        errors.content = 'Enter a content!';
+    }
+
+    if (!values.categories){
+        errors.categories = 'Enter a categories!';
+    }
+    
+    //if error is empty, the form is fine to submit 
+    //if error has *any* properties, redux form is assumes form is invalid
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(PostsNew);
 
